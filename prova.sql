7fcd02caf8f3d2b223223acd22d5928d02ab4173
Iara <loure.iara.s@gmail.com>
2023-09-26 09:55:40 -0300

    feat(cursor-bound): obtenção de descrição mais longa por país

--- prova.sql
+++ prova.sql
@@ -1,31 +1,53 @@
+-- Identificar a descrição mais longa para os vinhos de cada país utilizando um cursor vinculado.
 DO $$
 DECLARE
-  cur_media_prec REFCURSOR;
-  v_country VARCHAR(200);
-  v_price NUMERIC := 0; -- Alterei o tipo de dado para NUMERIC, adequado para preços decimais
-  v_nome_tabela VARCHAR(200) := 'vinho';
+v_country VARCHAR(200);
+v_description VARCHAR(1000);
+cur_country_descri CURSOR FOR SELECT country, description FROM
+vinho ORDER BY country, LENGTH(description) DESC ;
 BEGIN
-  OPEN cur_media_prec FOR EXECUTE
-    format(
-      'SELECT
-       country,
-       AVG(price)
-       FROM
-       %s
-       GROUP BY country', -- Adicionei GROUP BY para calcular a média por país
-      v_nome_tabela
-    );
-
-  LOOP
-    FETCH cur_media_prec INTO v_country, v_price;
-    EXIT WHEN NOT FOUND;
-
-    RAISE NOTICE '%: %.2f', v_country, v_price; -- Exibindo o preço médio com duas casas decimais
-  END LOOP;
-
-  CLOSE cur_media_prec;
+	OPEN cur_country_descri;
+	LOOP
+		FETCH cur_country_descri INTO v_country, v_description;
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE '%: %', v_country, v_description;
+	END LOOP;
+	CLOSE cur_country_descri;
 END;
 $$
+
+
+
+--Calcular o preço médio dos vinhos de cada país utilizando um cursor não vinculado.
+
+-- DO $$
+-- DECLARE
+--   cur_media_prec REFCURSOR;
+--   v_country VARCHAR(200);
+--   v_price NUMERIC := 0; -- Alterei o tipo de dado para NUMERIC, adequado para preços decimais
+--   v_nome_tabela VARCHAR(200) := 'vinho';
+-- BEGIN
+--   OPEN cur_media_prec FOR EXECUTE
+--     format(
+--       'SELECT
+--        country,
+--        AVG(price)
+--        FROM
+--        %s
+--        GROUP BY country', -- Adicionei GROUP BY para calcular a média por país
+--       v_nome_tabela
+--     );
+
+--   LOOP
+--     FETCH cur_media_prec INTO v_country, v_price;
+--     EXIT WHEN NOT FOUND;
+
+--     RAISE NOTICE '%: %.2f', v_country, v_price; -- Exibindo o preço médio com duas casas decimais
+--   END LOOP;
+
+--   CLOSE cur_media_prec;
+-- END;
+-- $$
 
 
 
